7861c4ca309ed5c44125b98fb37f4fec1785333d
Leena <Leena@DESKTOP-7UOEN22>
2022-04-25 14:51:17 +0530

    by leena

--- Githubdem2/src/com/cjc12/Githubclass.java
+++ Githubdem2/src/com/cjc12/Githubclass.java
@@ -1,8 +1,15 @@
 package com.cjc12;
 
 public class Githubclass {
+	
+	public void m1()
+	{
+		System.out.println("M1--");
+	}
 	public static void main(String[] args) {
 		System.out.println("hello");
+		Githubclass g=new Githubclass();
+		g.m1();
 	}
 
 }
